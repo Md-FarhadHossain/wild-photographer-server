b1abc856ca3d047ddb03c6106dc4f4fef4f17edc
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2022-11-08 05:03:19 +0600

    dynamic route set of services section

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require("cors")
     require('colors')
     require('dotenv').config()
@@ -46,10 +46,21 @@ const servicesCollection = client.db('servicesDB').collection('servicesList')
 app.get('/services', async(req, res) => {
     try {
        const limit = req.headers.limit
-       console.log(parseInt(limit))
+       
         const query = {}
         const cursor = servicesCollection.find(query).limit(parseInt(limit))
         const result = await cursor.toArray()
+        res.send(result)
+    }
+    catch(error){
+        console.log(error.name.bgRed, error.message.bold)
+    }
+})
+
+app.get('/services/:id', async(req, res) => {
+    try {
+        const id = req.params.id
+        const result = await servicesCollection.findOne({ _id: ObjectId(id) })
         res.send(result)
     }
     catch(error){
