713f10c211dbf0d43b8b3ff2c7036d3e9b32079a
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2022-11-10 02:24:25 +0600

    jwt post method created

--- index.js
+++ index.js
@@ -1,68 +1,66 @@
-const express = require("express")
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const cors = require("cors")
-    require('colors')
-    require('dotenv').config()
-const app = express()
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("colors");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-
-
-app.use(cors())
-app.use(express.json())
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.1ivadd4.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-const dbConnect = async() => {
-    try{
-        await client.connect();
-        console.log('database connected')
-    }
-    catch(error){
-        console.log(error.name.bgRed, error.message.bold)
-    }
-}
-dbConnect()
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const dbConnect = async () => {
+  try {
+    await client.connect();
+    console.log("database connected");
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+  }
+};
+dbConnect();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-const servicesCollection = client.db('servicesDB').collection('servicesList')
-const reviewsColleciton = client.db('servicesDB').collection('reviewList')
+const servicesCollection = client.db("servicesDB").collection("servicesList");
+const reviewsColleciton = client.db("servicesDB").collection("reviewList");
 
-app.post('/add-review', async(req, res) => {
-    try {
-        const review = req.body
-        console.log(review)
-        const result = await reviewsColleciton.insertOne(review)
-        res.send(result)
-        console.log(result)
+app.post("/add-review", async (req, res) => {
+  try {
+    const review = req.body;
+    console.log(review);
+    const result = await reviewsColleciton.insertOne(review);
+    res.send(result);
+    console.log(result);
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+  }
+});
+app.get("/add-review", async (req, res) => {
+  try {
+    let query = {};
+    if (req.query.email) {
+      query = { email: req.query.email };
     }
-    catch(error){
-        console.log(error.name.bgRed, error.message.bold)
+    if (req.query.serviceId) {
+      query = { serviceId: req.query.serviceId };
     }
-})
-app.get('/add-review', async(req, res) => {
-    try{
-        let query = {}
-        if(req.query.email){
-            query = { email: req.query.email}
-        }
-        if(req.query.serviceId){
-            query = { serviceId: req.query.serviceId}
-        }
-        const cursor = reviewsColleciton.find(query)
-        const result = await cursor.toArray()
-        res.send(result)
-    }
-    catch(error){
-        console.log(error.name.bgRed, error.message.bold)
-    }
-})
+    const cursor = reviewsColleciton.find(query);
+    const result = await cursor.toArray();
+    res.send(result);
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+  }
+});
 
 // app.get('/add-review/:id', async(req, res) => {
 //     try {
@@ -79,74 +77,85 @@ app.get('/add-review', async(req, res) => {
 //         console.log(error.name.bgRed, error.message.bold)
 //     }
 // })
-app.get('/services', async(req, res) => {
+app.get("/services", async (req, res) => {
+  try {
+    const limit = req.headers.limit;
+    const sort = { _id: -1 };
+    const query = {};
+    const cursor = servicesCollection
+      .find(query)
+      .limit(parseInt(limit))
+      .sort(sort);
+    const result = await cursor.toArray();
+    res.send(result);
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+  }
+});
+
+app.get("/services/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await servicesCollection.findOne({ _id: ObjectId(id) });
+    res.send(result);
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+  }
+});
+app.post("/services", async (req, res) => {
+  try {
+    const result = await servicesCollection.insertOne(req.body);
+    res.send(result);
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+  }
+});
+app.delete("/add-review/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const result = await reviewsColleciton.deleteOne(query);
+    console.log(result);
+    res.send(result);
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+  }
+});
+app.get("/add-review/:id", async (req, res) => {
     try {
-       const limit = req.headers.limit
-       const sort = {_id: -1} 
-        const query = {}
-        const cursor = servicesCollection.find(query).limit(parseInt(limit)).sort(sort)
-        const result = await cursor.toArray()
-        res.send(result)
+      console.log(req.headers.authorization)
+    const id = req.params.id;
+    const result = await reviewsColleciton.findOne({ _id: ObjectId(id) });
+    res.send(result);
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+  }
+});
+app.patch("/add-review/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await reviewsColleciton.updateOne(
+      { _id: ObjectId(id) },
+      { $set: req.body }
+    );
+    res.send(result);
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+  }
+});
+
+app.post('/jwt', (req, res) => {
+    try {
+        const user = req.body
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+        res.send({token})
+        // console.log(result)
     }
-    catch(error){
-        console.log(error.name.bgRed, error.message.bold)
-    }
+    catch (error) {
+        console.log(error.name.bgRed, error.message.bold);
+      }
 })
 
-app.get('/services/:id', async(req, res) => {
-    try {
-        const id = req.params.id
-        const result = await servicesCollection.findOne({ _id: ObjectId(id) })
-        res.send(result)
-    }
-    catch(error){
-        console.log(error.name.bgRed, error.message.bold)
-    }
-})
-app.post('/services', async(req, res) => {
-   try {
-    
-    const result = await servicesCollection.insertOne(req.body)
-    res.send(result)
-   }
-   catch(error){
-    console.log(error.name.bgRed, error.message.bold)
-}
-})
-app.delete('/add-review/:id', async(req, res) => {
-    try {
-        const id = req.params.id
-        const query = {_id: ObjectId(id)}
-        const result = await reviewsColleciton.deleteOne(query)
-        console.log(result)
-        res.send(result)
-    }
-    catch(error){
-        console.log(error.name.bgRed, error.message.bold)
-    }
-})
-app.get('/add-review/:id', async(req, res) => {
-    try {
-        const id = req.params.id
-        const result = await reviewsColleciton.findOne({ _id: ObjectId(id) })
-        res.send(result)
-    }
-    catch(error){
-        console.log(error.name.bgRed, error.message.bold)
-    }
-})
-app.patch('/add-review/:id', async(req, res) => {
-    try {
-        const id = req.params.id
-       const result = await reviewsColleciton.updateOne({_id: ObjectId(id)}, {$set: req.body})
-       res.send(result)
-
-    }
-    catch(error){
-        console.log(error.name.bgRed, error.message.bold)
-    }
-})
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
-})
-
+  console.log(`Server listening on port ${PORT}`);
+});
